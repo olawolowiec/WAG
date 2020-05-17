9f25954e75e861039332aa9c64052be4375d4a50
mmarganski <62937020+mmarganski@users.noreply.github.com>
2020-05-17 12:22:10 +0200

    Update wag.py

--- wag.py
+++ wag.py
@@ -10,7 +10,7 @@ class Error:
 	def as_string(self):
 		result  = f'{self.error_name}: {self.details}\n'
 		result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
-		result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+		result += '\n\n' 
 		return result
 
 class IllegalCharError(Error):
@@ -29,7 +29,7 @@ class RTError(Error):
 	def as_string(self):
 		result  = self.generate_traceback()
 		result += f'{self.error_name}: {self.details}'
-		result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+		result += '\n\n' 
 		return result
 
 	def generate_traceback(self):
